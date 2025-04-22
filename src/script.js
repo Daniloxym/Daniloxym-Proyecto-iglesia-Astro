e3412f63621c9db2d1f6525ca6a07a7b2e96813c
Danilo Olivos <daniolip2021@gmail.com>
2025-04-21 21:20:55 -0500

    Mejoras de rendimiento

---
+++ src/script.js
@@ -0,0 +1,13 @@
+//Creacion de botones para abrir y cerrar navbar
+const menuCloseButton = document.querySelector(".navegacion__cerrar");
+const menuOpenButton = document.querySelector(".navegacion__bars");
+
+menuCloseButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+  document.body.classList.toggle("efecto");
+});
+
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.toggle("show-mobile-menu");
+  document.body.classList.toggle("efecto");
+});
