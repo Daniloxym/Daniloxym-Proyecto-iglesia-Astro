955cc88da152ca9baf21e76e517cad2317835678
Danilo Olivos <daniolip2021@gmail.com>
2025-10-15 08:13:41 -0500

    created predicas page

---
+++ src/utils/constants.ts
@@ -0,0 +1,77 @@
+import type { Predicas } from '../types';
+import predica from '@/assets/img/predica2.png';
+import predica3 from '@/assets/img/predica3.png';
+import predica4 from '@/assets/img/predica4.png';
+
+export const predicas: Predicas[] = [
+  {
+    id: 'ysz5S6PUM-U',
+    title: 'Dios Tiene un Plan Perfecto para Ti',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Mike Riffle',
+    date: '2024-07-30'
+  },
+  {
+    id: 'tgbNymZ7vqY',
+    title: 'Sanando las Heridas del Pasado',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Fabio Ledezma',
+    date: '2025-07-22'
+  },
+  {
+    id: 'ktvTqknDobU',
+    title: 'El Silencio de Dios',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Mike Riffle',
+    date: '2023-08-19'
+  },
+  {
+    id: 'aqz-KE-bpKQ',
+    title: 'El Poder de la Oración Persistente',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Fabio Ledezma',
+    date: '2025-06-20'
+  },
+  {
+    id: 'J---aiyznGQ',
+    title: 'Cuando No Entiendes el Proceso',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Mike Riffle',
+    date: '2024-02-12'
+  },
+  {
+    id: 'ScMzIvxBSi4',
+    title: 'La Gracia que Libera | Ana Milena',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Fabio Ledezma',
+    date: '2023-07-25'
+  },
+  {
+    id: 'sBws8MSXN7A',
+    title: 'Fe en Medio del Desierto ',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Mike Riffle',
+    date: '2025-05-03'
+  },
+  {
+    id: 'e-ORhEE9VVg',
+    title: 'El Llamado del Espíritu Santo ',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Fabio Ledezma',
+    date: '2025-06-18'
+  },
+  {
+    id: '9bZkp7q19f0',
+    title: 'La Voz de Dios en el Silencio',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Mike Riffle',
+    date: '2025-10-25'
+  },
+  {
+    id: 'uelHwf8o7_U',
+    title: 'Camina Sobre las Aguas de la Fe',
+    thumbnail: predica4.src,
+    preacher: 'Pastor Fabio Ledezma',
+    date: '2025-05-06'
+  }
+];
---
+++ src/utils/pagination.ts
@@ -0,0 +1,88 @@
+import { predicas } from './constants';
+
+const predicasLista = document.querySelector('.predicas__lista') as HTMLDivElement;
+const paginationContainer = document.querySelector('.pagination') as HTMLDivElement;
+const pageInfo = document.getElementById('page-info') as HTMLSpanElement;
+
+const itemsPerPage = 6;
+let currentPage = 1;
+const totalPages = Math.ceil(predicas.length / itemsPerPage);
+
+function renderPreaches() {
+  predicasLista.innerHTML = '';
+  const start = (currentPage - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+
+  const preachesToDisplay = predicas.slice(start, end);
+
+  preachesToDisplay.forEach((preach) => {
+    const preachElement = document.createElement('div');
+    preachElement.classList.add('predicas__item');
+    preachElement.innerHTML = `
+      <div style="background-image: url('${preach.thumbnail}')" class="predicas__thumbnail"></div>
+      <h2 class="predicas__title">${preach.title}</h2>
+      <div class="predicas__info"><p class="predicas__preacher">${preach.preacher}</p>
+      <p class="predicas__date">${preach.date}</p></div>
+    `;
+    preachElement.onclick = () => openModal(preach.id);
+
+    predicasLista.appendChild(preachElement);
+  });
+}
+
+function setupPagination() {
+  paginationContainer.innerHTML = '';
+
+  const prevButton = document.createElement('button');
+  prevButton.textContent = 'Anterior';
+  prevButton.classList.add('pagination__button');
+  prevButton.disabled = currentPage === 1;
+  prevButton.onclick = () => {
+    if (currentPage <= 1) return;
+    currentPage--;
+    renderPreaches();
+    setupPagination();
+  };
+  paginationContainer.appendChild(prevButton);
+
+  pageInfo.textContent = `Página ${currentPage} de ${totalPages}`;
+  paginationContainer.appendChild(pageInfo);
+
+  // for (let i = 1; i <= totalPages; i++) {
+  //   const pageButton = document.createElement('button');
+  //   pageButton.textContent = i.toString();
+  //   pageButton.classList.add('pagination__button');
+  //   pageButton.classList.toggle('active', i === currentPage);
+  //   pageButton.addEventListener('click', () => {
+  //     currentPage = i;
+  //     renderPreaches();
+  //     setupPagination();
+  //   });
+  //   paginationContainer.appendChild(pageButton);
+  // }
+
+  const nextButton = document.createElement('button');
+  nextButton.textContent = 'Siguiente';
+  nextButton.classList.add('pagination__button');
+  nextButton.disabled = currentPage === totalPages;
+  nextButton.onclick = () => {
+    if (currentPage >= totalPages) return;
+    currentPage++;
+    renderPreaches();
+    setupPagination();
+  };
+  paginationContainer.appendChild(nextButton);
+}
+
+function openModal(videoId) {
+  videoFrame.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&modestbranding=1`;
+  modal.classList.add('active');
+}
+
+closeModal.addEventListener('click', () => {
+  modal.classList.remove('active');
+  videoFrame.src = '';
+});
+
+setupPagination();
+renderPreaches();
