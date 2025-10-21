565cecf7144ec7186ff1cea5652f23c34cc67284
Danilo Olivos <daniolip2021@gmail.com>
2025-10-21 11:44:44 -0500

    update list sermons

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,68 +8,68 @@ export const predicas: Predicas[] = [
     thumbnail: `https://img.youtube.com/vi/T-ebvJYsTRY/maxresdefault.jpg`,
     preacher: 'Pastor Mike Riffle',
     date: '19 de Octubre del 2025'
-  },
-  {
-    id: 'tgbNymZ7vqY',
-    title: 'Sanando las Heridas del Pasado',
-    thumbnail: predica.src,
-    preacher: 'Pastor Fabio Ledezma',
-    date: '2025-07-22'
-  },
-  {
-    id: 'ktvTqknDobU',
-    title: 'El Silencio de Dios',
-    thumbnail: predica.src,
-    preacher: 'Pastor Mike Riffle',
-    date: '2023-08-19'
-  },
-  {
-    id: 'aqz-KE-bpKQ',
-    title: 'El Poder de la Oración Persistente',
-    thumbnail: predica.src,
-    preacher: 'Pastor Fabio Ledezma',
-    date: '2025-06-20'
-  },
-  {
-    id: 'J---aiyznGQ',
-    title: 'Cuando No Entiendes el Proceso',
-    thumbnail: predica.src,
-    preacher: 'Pastor Mike Riffle',
-    date: '2024-02-12'
-  },
-  {
-    id: 'ScMzIvxBSi4',
-    title: 'La Gracia que Libera | Ana Milena',
-    thumbnail: predica.src,
-    preacher: 'Pastor Fabio Ledezma',
-    date: '2023-07-25'
-  },
-  {
-    id: 'sBws8MSXN7A',
-    title: 'Fe en Medio del Desierto ',
-    thumbnail: predica.src,
-    preacher: 'Pastor Mike Riffle',
-    date: '2025-05-03'
-  },
-  {
-    id: 'e-ORhEE9VVg',
-    title: 'El Llamado del Espíritu Santo ',
-    thumbnail: predica.src,
-    preacher: 'Pastor Fabio Ledezma',
-    date: '2025-06-18'
-  },
-  {
-    id: '9bZkp7q19f0',
-    title: 'La Voz de Dios en el Silencio',
-    thumbnail: predica.src,
-    preacher: 'Pastor Mike Riffle',
-    date: '2025-10-25'
-  },
-  {
-    id: 'uelHwf8o7_U',
-    title: 'Camina Sobre las Aguas de la Fe',
-    thumbnail: predica.src,
-    preacher: 'Pastor Fabio Ledezma',
-    date: '2025-05-06'
   }
+  // {
+  //   id: 'tgbNymZ7vqY',
+  //   title: 'Sanando las Heridas del Pasado',
+  //   thumbnail: predica.src,
+  //   preacher: 'Pastor Fabio Ledezma',
+  //   date: '2025-07-22'
+  // },
+  // {
+  //   id: 'ktvTqknDobU',
+  //   title: 'El Silencio de Dios',
+  //   thumbnail: predica.src,
+  //   preacher: 'Pastor Mike Riffle',
+  //   date: '2023-08-19'
+  // },
+  // {
+  //   id: 'aqz-KE-bpKQ',
+  //   title: 'El Poder de la Oración Persistente',
+  //   thumbnail: predica.src,
+  //   preacher: 'Pastor Fabio Ledezma',
+  //   date: '2025-06-20'
+  // },
+  // {
+  //   id: 'J---aiyznGQ',
+  //   title: 'Cuando No Entiendes el Proceso',
+  //   thumbnail: predica.src,
+  //   preacher: 'Pastor Mike Riffle',
+  //   date: '2024-02-12'
+  // },
+  // {
+  //   id: 'ScMzIvxBSi4',
+  //   title: 'La Gracia que Libera | Ana Milena',
+  //   thumbnail: predica.src,
+  //   preacher: 'Pastor Fabio Ledezma',
+  //   date: '2023-07-25'
+  // },
+  // {
+  //   id: 'sBws8MSXN7A',
+  //   title: 'Fe en Medio del Desierto ',
+  //   thumbnail: predica.src,
+  //   preacher: 'Pastor Mike Riffle',
+  //   date: '2025-05-03'
+  // },
+  // {
+  //   id: 'e-ORhEE9VVg',
+  //   title: 'El Llamado del Espíritu Santo ',
+  //   thumbnail: predica.src,
+  //   preacher: 'Pastor Fabio Ledezma',
+  //   date: '2025-06-18'
+  // },
+  // {
+  //   id: '9bZkp7q19f0',
+  //   title: 'La Voz de Dios en el Silencio',
+  //   thumbnail: predica.src,
+  //   preacher: 'Pastor Mike Riffle',
+  //   date: '2025-10-25'
+  // },
+  // {
+  //   id: 'uelHwf8o7_U',
+  //   title: 'Camina Sobre las Aguas de la Fe',
+  //   thumbnail: predica.src,
+  //   preacher: 'Pastor Fabio Ledezma',
+  //   date: '2025-05-06'
+  // }
 ];
